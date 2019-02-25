7b82bb129cefe723979d1140c849c2cec5736123
Stephen Wright <scw248@gmail.com>
2019-02-25 21:31:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,9 @@ class App < Sinatra::Base
   
   get "/multiply/:num1/:num2" do
     #binding.pry
-    @result = params[:num1 *= :num2]
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    @result = @num1 * @num2
     @result
   end
 
