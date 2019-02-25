6980745971d3f1f468fd0024f665d0318ce48440
Stephen Wright <scw248@gmail.com>
2019-02-25 21:30:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  get "/multiply/num1/num2" do
+  get "/multiply/:num1/:num2" do
     #binding.pry
     @result = params[num1 * num2]
     @result
